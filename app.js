ea614a47b0931f720437a6e6408f5c90f611b04a
kyle-davie <143517842+kyle-davie@users.noreply.github.com>
2023-10-18 11:22:36 -0500

    login forum

---
+++ app.js
@@ -0,0 +1,16 @@
+
+function verify() {
+  let userLogin = document.getElementById("login-user").value;
+  let userPassword = document.getElementById("login-pass").value;
+  let username = 'Admin';
+  let password = 'Password'
+
+  if (userLogin == username && userPassword == password) {
+    document.getElementById("status").innerText = "Login Successful"
+    window.location.assign("/success.html")
+    
+  } else {
+    document.getElementById("status").innerText = "failed";
+    console.log(userLogin);
+  }
+}
